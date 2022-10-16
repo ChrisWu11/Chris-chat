e190e63d0e1ed85cbf2af36fed309c49cc9edb9b
wukang <893276941@qq.com>
2022-10-17 00:09:35 +0800

    实现图片和信息展示

---
+++ commons/datas.js
@@ -0,0 +1,63 @@
+export default{
+	message:function(){
+		let msg = [
+			{
+				id:'a',
+				imgurl:'one.png',
+				message:'长沙市测试测试测试测试测试测试测试测试',
+				types:0,
+				time:new Date()-1000,
+				tip:0,
+			},
+			{
+				id:'a',
+				imgurl:'one.png',
+				message:'行i偶的死哦改价哦i的设计风格iops的窘迫分给色破官方',
+				types:0,
+				time:new Date()-1000*16,
+				tip:1,
+			},
+			{
+				id:'b',
+				imgurl:'two.png',
+				message:'设计风格iops的迫分给色破官方',
+				types:0,
+				time:new Date()-1000*60,
+				tip:2,
+			},
+			{
+				id:'a',
+				imgurl:'one.png',
+				message:'one.png',
+				types:1,
+				time:new Date()-1000*60*36,
+				tip:3,
+			},
+			{
+				id:'a',
+				imgurl:'one.png',
+				message:'561651651651',
+				types:0,
+				time:new Date()-1000*60*40,
+				tip:4,
+			},
+			{
+				id:'b',
+				imgurl:'two.png',
+				message:'si.jpg',
+				types:1,
+				time:new Date()-1000*57,
+				tip:5,
+			},
+			{
+				id:'a',
+				imgurl:'two.png',
+				message:'yi.jpg',
+				types:1,
+				time:new Date()-1000*58,
+				tip:6,
+			},
+		];
+		return msg;
+	}
+}
